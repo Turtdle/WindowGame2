05b6323329f0947cbb0b391b6ff5f589978e7559
Turtdle <sam@wanfamily.org>
2025-04-28 14:18:11 -0700

    :)

--- character.py
+++ character.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 class Character:
     def __init__(self, x=100, y=100, size=50, color=(0, 255, 0), speed=5):
@@ -9,7 +10,7 @@ class Character:
         self.speed = speed
         self.vx = 0
         self.vy = 0
-        self.current_level = None  # Track which level we're in
+        self.current_level = None 
 
     def set_level(self, level_name):
         self.current_level = level_name
@@ -19,16 +20,47 @@ class Character:
         self.y += self.vy
 
     def draw(self, screen):
+       
         pygame.draw.rect(
             screen,
             self.color,
             (self.x, self.y, self.size, self.size)
         )
+        
 
+        eye_radius = self.size // 10
+        eye_offset_x = self.size // 4
+        eye_offset_y = self.size // 3
+
+        pygame.draw.circle(
+            screen,
+            (0, 0, 0), 
+            (self.x + eye_offset_x, self.y + eye_offset_y),
+            eye_radius
+        )
+        
+
+        pygame.draw.circle(
+            screen,
+            (0, 0, 0), 
+            (self.x + self.size - eye_offset_x, self.y + eye_offset_y),
+            eye_radius
+        )
+        
+
+        smile_radius = self.size // 3
+        pygame.draw.arc(
+            screen,
+            (0, 0, 0), 
+            (self.x + self.size//4, self.y + self.size//2, self.size//2, self.size//3),
+            math.pi, 
+            2 * math.pi, 
+            2  
+        )
 
     def handle_keys(self):
         keys = pygame.key.get_pressed()
-        self.vx = 0  # Always reset horizontal velocity
+        self.vx = 0
         
         if self.current_level == "Level2" or self.current_level == "Level3":
             if keys[pygame.K_LEFT] or keys[pygame.K_a]:
@@ -36,7 +68,7 @@ class Character:
             if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
                 self.vx = self.speed
         else:
-            self.vy = 0  # Reset vertical velocity
+            self.vy = 0  
             
             if keys[pygame.K_LEFT] or keys[pygame.K_a]:
                 self.vx = -self.speed
